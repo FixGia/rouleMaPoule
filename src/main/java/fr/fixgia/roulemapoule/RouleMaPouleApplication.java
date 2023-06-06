4a27b992cf7a6f27e8e6d045f75a45ca1f822deb
fxgiracca <fxgiracca@itroom.fr>
2023-06-06 09:11:30 +0200

    Feature(Initialisation du projet RouleMaPouleApplication

---
+++ src/main/java/fr/fixgia/roulemapoule/RouleMaPouleApplication.java
@@ -0,0 +1,13 @@
+package fr.fixgia.roulemapoule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouleMaPouleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RouleMaPouleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/fixgia/roulemapoule/RouleMaPouleApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fixgia.roulemapoule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouleMaPouleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
