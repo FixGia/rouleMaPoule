47074d3e15327e1dda9b9fa94cc77e4768b8d378
fxgiracca <fxgiracca@itroom.fr>
2023-06-06 15:42:32 +0200

    feature(nouveau correctif lié au controller trajet)

--- src/main/java/fr/fixgia/roulemapoule/trajet/Trajet.java
+++ src/main/java/fr/fixgia/roulemapoule/trajet/Trajet.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 public class Trajet {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     UUID id;
     @Column
     String ville_depart;
